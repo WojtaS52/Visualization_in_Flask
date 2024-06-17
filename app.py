1ececabd88756e1d6d67763f2ea98fe5042f4de9
Wojciech Świderski <wojciechiderski@gmail.com>
2024-06-18 01:48:21 +0200

    inital commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, jsonify
+import requests
+
+app = Flask(__name__)
+
+API_URL = 'https://restcountries.com/v3.1/all'
+
+EUROPEAN_COUNTRIES = [
+    'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'IS', 'IE', 'IT',
+    'LV', 'LT', 'LU', 'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'CH', 'GB'
+]
+
+def get_country_data():
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/currencies')
+def get_currencies():
+    countries = get_country_data()
+    currencies = {}
+    for country in countries:
+        if country['cca2'] in EUROPEAN_COUNTRIES:
+            currencies[country['cca2']] = {
+                'name': country['name']['common'],
+                'currency': list(country['currencies'].values())[0]['name'] if 'currencies' in country else 'No currency data',
+                'latlng': country['latlng'] if 'latlng' in country else [0, 0]
+            }
+    return jsonify(currencies)
+
+if __name__ == '__main__':
+    app.run(debug=True)
